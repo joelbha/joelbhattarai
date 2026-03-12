69ed2cfa348c1dd00f2fd0ee86f7c27447a09f1c
joelbha <joelbh@umich.edu>
2026-03-12 00:35:30 -0400

    Clean up comment in scroll event listener
    Removed unnecessary comment from scroll event listener.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", function() {
     const nav = document.querySelector("nav");
     
-    // If we scroll down more than 50px, add the 'shrunk' class
+    
     if (window.scrollY > 50) {
         nav.classList.add("shrunk");
     } else {
