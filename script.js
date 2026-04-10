62fd39b9e43dddcf5bf099d45100dc5de2e538ed
yijo2004 <yijo@umich.edu>
2026-04-10 17:03:43 -0400

    Added mobile version, added github actions for pdf generation

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ window.addEventListener("scroll", function() {
         nav.classList.remove("shrunk");
     }
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navUl = document.querySelector("nav ul");
+
+hamburger.addEventListener("click", function() {
+    hamburger.classList.toggle("open");
+    navUl.classList.toggle("nav-open");
+});
+
+navUl.querySelectorAll("a").forEach(function(link) {
+    link.addEventListener("click", function() {
+        hamburger.classList.remove("open");
+        navUl.classList.remove("nav-open");
+    });
+});
